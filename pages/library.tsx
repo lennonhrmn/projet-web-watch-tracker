5e0aaed52c870192d486e916e26ddf8d0ca04625
Lennon Herrmann <lennonherrmann@hotmail.com>
2024-05-25 23:15:19 +0200

    maj

--- pages/library.tsx
+++ pages/library.tsx
@@ -79,8 +79,8 @@ const Library = () => {
                         <h1 className="mr-3">Welcome {name}</h1>
                         <MdWavingHand className="mt-1" />
                     </div>
-                    <div>
-                        <div className="ml-10 p-5 flex gap-8">
+                    <div className='ml-10'>
+                        <div className="p-5 flex gap-8">
                             <h1 className="text-white sm:text-2xl md:text-3xl">My Library</h1>
                             <select id="categorySelect"
                                 className="cursor-pointer sm:text-1xl md:text-2xl mt-1 text-white bg-black hover:text-gray-300 translation"
@@ -90,7 +90,7 @@ const Library = () => {
                                 <option value="Manga">Manga</option>
                             </select>
                         </div>
-                        <div className="ml-10 p-5">
+                        <div className="p-5">
                             <select id="favoriteStatus"
                                 className="cursor-pointer sm:text-1xl md:text-2xl mt-1 text-white bg-black hover:text-gray-300 translation"
                                 onChange={handleStatusChange}
@@ -99,6 +99,10 @@ const Library = () => {
                                 <option value="currentlyWatching">Currently Watching</option>
                                 <option value="finishedWatching">Finished Watching</option>
                             </select>
+                        </div>
+                        <div className='text-white p-5 space-y-1'>
+                            <p>No favorites at the moment, what are you waiting for !</p>
+                            <p>Go check content by clicking on category or the search bar ;)</p>
                         </div>
                     </div>
                 </div>
